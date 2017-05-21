efeeb0ac5791f24a62df0ea0931767e2f2898b85
Weird Constructor <weirdconstructor@gmail.com>
2017-05-21 07:29:50 +0200

    Forgot to check in the main files.

--- main.cpp
+++ main.cpp
@@ -1,12 +1,35 @@
 #include <cstdio>
+#include <fstream>
+#include "lilvm.h"
 #include "JSON.h"
+
+//---------------------------------------------------------------------------
+
+using namespace lilvm;
+using namespace std;
 
 class LILASMParser : public json::Parser
 {
   private:
+      enum State
+      {
+          INIT,
+          READ_PROG,
+          READ_OP
+      };
+
+      State     m_state;
+
+      lilvm::Operation *m_cur_op;
+      lilvm::VM        &m_vm;
+
 
   public:
-    LILASMParser() {}
+    LILASMParser(lilvm::VM &vm)
+        : m_state(INIT),
+          m_cur_op(nullptr),
+          m_vm(vm)
+    {}
     virtual ~LILASMParser() { }
 
     virtual void onObjectStart()                      {}
@@ -14,21 +37,217 @@ class LILASMParser : public json::Parser
     virtual void onObjectValueDone()                  { }
     virtual void onObjectEnd()                        {}
 
-    virtual void onArrayStart()                       {}
-    virtual void onArrayValueDone()                   {}
-    virtual void onArrayEnd()                         {}
-    virtual void onError(UTF8Buffer *u8Buf, const char *csError) { (void) u8Buf; (void) csError; }
+    virtual void onArrayStart()
+    {
+        switch (m_state)
+        {
+            case INIT: // opening "[" in the file
+                m_state = READ_PROG;
+                break;
+
+            case READ_PROG:
+                m_state = READ_OP;
+                m_cur_op = new Operation(NOP);
+                break;
+
+            default:
+                // Ignore it boldly
+                break;
+        }
+    }
+
+    virtual void onArrayValueDone()
+    {
+    }
+
+    virtual void onArrayEnd()
+    {
+        switch (m_state)
+        {
+            case READ_OP: // opening "[" in the file
+                m_vm.append(m_cur_op);
+                m_cur_op = nullptr;
+
+                m_state = READ_PROG;
+                break;
+
+            case READ_PROG:
+                // TODO: do something useful?
+                m_state = INIT;
+                break;
+
+            default:
+                // Ignore it boldly
+                break;
+        }
+    }
+    virtual void onError(UTF8Buffer *u8Buf, const char *csError)
+    {
+        cout << "ERROR: Parsing the JSON: " << csError << endl;
+    }
 
     virtual void onValueBoolean(bool bValue) { (void) bValue; }
     virtual void onValueNull()               {}
-    virtual void onValueNumber(const char *csNumber, bool bIsFloat) { (void) csNumber; (void) bIsFloat; }
-    virtual void onValueString(const std::string &sString)          { (void) sString; }
+    virtual void onValueNumber(const char *csNumber, bool bIsFloat)
+    {
+        if (!m_cur_op)
+        {
+            cout << "ERROR: Bad OP sequence in JSON, bad OP type?" << endl;
+            return;
+        }
+
+        if (bIsFloat)
+            m_cur_op->m_1.d = stod(csNumber);
+        else
+            m_cur_op->m_1.i = stoll(csNumber);
+    }
+
+    virtual void onValueString(const std::string &sString)
+    {
+        if (!m_cur_op)
+        {
+            cout << "ERROR: Can't define OP type at this point!" << endl;
+            return;
+        }
+
+#define HANDLE_OP_TYPE(type) \
+        else if (sString == #type) m_cur_op->m_op = type;
+
+        if (sString == "") m_cur_op->m_op = NOP;
+        HANDLE_OP_TYPE(NOP)
+        HANDLE_OP_TYPE(PUSH_I)
+        HANDLE_OP_TYPE(PUSH_D)
+        HANDLE_OP_TYPE(DBG_DUMP_STACK)
+        HANDLE_OP_TYPE(ADD_I)
+        HANDLE_OP_TYPE(ADD_D)
+        HANDLE_OP_TYPE(GOTO)
+        HANDLE_OP_TYPE(BRANCH_IF)
+        HANDLE_OP_TYPE(LT_D)
+        HANDLE_OP_TYPE(LE_D)
+        HANDLE_OP_TYPE(GT_D)
+        HANDLE_OP_TYPE(GE_D)
+        HANDLE_OP_TYPE(LT_I)
+        HANDLE_OP_TYPE(LE_I)
+        HANDLE_OP_TYPE(GT_I)
+        HANDLE_OP_TYPE(GE_I)
+        HANDLE_OP_TYPE(DEF_ARGS)
+        HANDLE_OP_TYPE(CALL)
+        HANDLE_OP_TYPE(RET)
+        else
+            cout << "ERROR: Unknown OP type: " << sString << endl;
+    }
 };
+//---------------------------------------------------------------------------
 
 int test();
+//---------------------------------------------------------------------------
+
+UTF8Buffer *slurp(const std::string &filepath)
+{
+    ifstream input_file(filepath.c_str(),
+                        ios::in | ios::binary | ios::ate);
+
+    if (input_file.is_open())
+    {
+        size_t size = input_file.tellg();
+
+        // FIXME (maybe, but not yet)
+        char *unneccesary_buffer_just_to_copy
+            = new char[size];
+
+        input_file.seekg(0, ios::beg);
+        input_file.read(unneccesary_buffer_just_to_copy, size);
+        input_file.close();
+
+        cout << "read(" << size << ")["
+             << unneccesary_buffer_just_to_copy << "]" << endl;
+
+        UTF8Buffer *u8b =
+            new UTF8Buffer(unneccesary_buffer_just_to_copy, size);
+        delete[] unneccesary_buffer_just_to_copy;
+
+        return u8b;
+    }
+    else
+    {
+        cout << "ERROR: Couldn't open: '" << filepath << endl;
+        return nullptr;
+    }
+}
+//---------------------------------------------------------------------------
 
 int main(int argc, char *argv[])
 {
-    printf("%d\n", test());
-    return 0;
+    using namespace std;
+
+    std::string input_file_path = "input.json";
+
+    if (argc > 1)
+        input_file_path = string(argv[1]);
+
+    try
+    {
+        VM vm;
+
+        LILASMParser theParser(vm);
+
+        UTF8Buffer *u8b = slurp(input_file_path);
+        if (!u8b)
+        {
+            cout << "ERROR: No input?!" << endl;
+            return 1;
+        }
+
+        try
+        {
+            theParser.parse(u8b);
+            delete u8b;
+        }
+        catch (UTF8Buffer_exception &e)
+        {
+            cout << "UTF-8 error!" << endl;
+            delete u8b;
+        }
+
+        vm.run();
+        return 0;
+    }
+    catch (const std::exception &e)
+    {
+        cout << "Exception: " << e.what() << endl;
+        return 1;
+    }
 }
+//---------------------------------------------------------------------------
+
+//        Operation *o = nullptr;
+//
+//        o = new Operation(NOP);
+//        vm.append(o);
+//
+//        o = new Operation(PUSH_I);
+//        o->m_1.i = 123;
+//        vm.append(o);
+//
+//        o = new Operation(PUSH_D);
+//        o->m_1.d = 432.12;
+//        vm.append(o);
+//
+//        o = new Operation(ADD_D);
+//        vm.append(o);
+//
+//        o = new Operation(PUSH_I);
+//        o->m_1.i = 123;
+//        vm.append(o);
+//
+//        o = new Operation(PUSH_D);
+//        o->m_1.d = 432.12;
+//        vm.append(o);
+//
+//        o = new Operation(ADD_I);
+//        vm.append(o);
+//
+//        o = new Operation(DBG_DUMP_STACK);
+//        vm.append(o);
+
+//---------------------------------------------------------------------------
--- src/lilvm.cpp
+++ src/lilvm.cpp
@@ -1,1 +1,178 @@
+#include "lilvm.h"
+
+using namespace std;
+
 int test() { return 11; }
+
+namespace lilvm
+{
+//---------------------------------------------------------------------------
+
+void VM::log(const string &error)
+{
+    cout << error << endl;
+}
+//---------------------------------------------------------------------------
+
+void VM::append(Operation *op)
+{
+    op->m_next = nullptr;
+
+    if (m_last)
+    {
+        m_last->m_next = op;
+        m_last = op;
+    }
+    else m_prog = m_last = op;
+}
+//---------------------------------------------------------------------------
+
+const char *OPCODE2NAME(OPCODE c)
+{
+    switch (c)
+    {
+        case NOP:            return "NOP";
+        case PUSH_I:         return "PUSH_I";
+        case PUSH_D:         return "PUSH_D";
+        case DBG_DUMP_STACK: return "DBG_DUMP_STACK";
+        case ADD_I:          return "ADD_I";
+        case ADD_D:          return "ADD_D";
+        case GOTO:           return "GOTO";
+        case BRANCH_IF:      return "BRANCH_IF";
+        case LT_D:           return "LT_D";
+        case LE_D:           return "LE_D";
+        case GT_D:           return "GT_D";
+        case GE_D:           return "GE_D";
+        case LT_I:           return "LT_I";
+        case LE_I:           return "LE_I";
+        case GT_I:           return "GT_I";
+        case GE_I:           return "GE_I";
+        case DEF_ARGS:       return "DEF_ARGS";
+        case CALL:           return "CALL";
+        case RET:            return "RET";
+        default:             return "unknown";
+    }
+}
+//---------------------------------------------------------------------------
+
+void VM::dump_stack(const std::string &msg)
+{
+    int offs = 0;
+
+    cout << "### BEGIN STACK DUMP {" << msg << "}" << endl;
+    if (m_stack_top == m_stack_bottom)
+        return;
+
+    Datum **stk_cur = m_stack_top;
+    do
+    {
+        stk_cur--;
+        Datum *d = *stk_cur;
+        cout << "    " << offs << " @" << stk_cur << ": " << d->to_string() << endl;
+        offs++;
+    }
+    while (stk_cur != m_stack_bottom);
+
+    cout << "### END STACK DUMP {" << msg << "}" << endl;
+}
+//---------------------------------------------------------------------------
+
+Datum *VM::new_dt_int(int64_t i)
+{
+    Datum *dt = new_datum(T_INT);
+    dt->m_d.i = i;
+    return dt;
+}
+//---------------------------------------------------------------------------
+
+Datum *VM::new_dt_dbl(double d)
+{
+    Datum *dt = new_datum(T_DBL);
+    dt->m_d.d = d;
+    return dt;
+}
+//---------------------------------------------------------------------------
+
+Datum *VM::new_datum(Type t)
+{
+    return new Datum(t);
+}
+//---------------------------------------------------------------------------
+
+void VM::run()
+{
+    m_cur = m_prog;
+
+    while (m_cur)
+    {
+        if (check_stack_overflow())
+        {
+            cout << "ERROR: STACK OVERFLOW!" << endl;
+            break;
+        }
+
+        cout << "(stks=" << GET_STK_SIZE << ") TRC: " << OPCODE2NAME(m_cur->m_op)
+             << endl;
+        Operation &op = *m_cur;
+
+        switch (op.m_op)
+        {
+            case NOP:
+                break;
+
+            case PUSH_I:
+            {
+                STK_PUSH(new_dt_int(op.m_1.i));
+                break;
+            }
+
+            case PUSH_D:
+            {
+                STK_PUSH(new_dt_dbl(op.m_1.d));
+                break;
+            }
+
+            case ADD_D:
+            {
+                Datum *new_dt =
+                    new_dt_dbl(  STK_AT(1)->to_dbl()
+                               + STK_AT(2)->to_dbl());
+                // TODO: Optimize, by pop only 1, and overwrite the next?
+                STK_POPN(2);
+                STK_PUSH(new_dt);
+
+                // FIXME: Implement mark & sweep garbage collector!?
+                break;
+            }
+
+            case ADD_I:
+            {
+                Datum *new_dt =
+                    new_dt_int(  STK_AT(1)->to_int()
+                               + STK_AT(2)->to_int());
+                // TODO: Optimize, by pop only 1, and overwrite the next?
+                STK_POPN(2);
+                STK_PUSH(new_dt);
+
+                // FIXME: Implement mark & sweep garbage collector!?
+                break;
+            }
+
+            case DBG_DUMP_STACK:
+            {
+                dump_stack("DBG_DUMP_STACK");
+                break;
+            }
+
+            default:
+                log("Unkown op: " + to_string(m_cur->m_op));
+                return;
+        }
+
+        m_cur = m_cur->m_next;
+
+    }
+}
+//---------------------------------------------------------------------------
+
+}
